3c8c3e3f5337326a7b2cbadc4ec53dccdc4c7ac8
Tom Christian <103541283+Thomas-Christian@users.noreply.github.com>
2022-08-09 19:04:57 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
+// DEPENDENCIES
 const express = require('express')
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
-console.log(PORT)
+const app = express()
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an Awesome App about Breads!')
+})
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
+
+// BREADS
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+  
